5e633f11e7e7efce804af6ac8a411f0dad152ad3
costadevsampaio-cmyk <costadevsampaio@gmail.com>
2026-03-20 20:26:11 -0300

    Add files via upload

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,12 @@
 import userController from '../controllers/user.controller.js';
-
+import authMiddleware from '../middlewares/auth.middleware.js';
 
 export default async function userRoutes(fastify) {
   fastify.post('/users', userController.create);
   fastify.post('/login', userController.login);
+
+  // 🔒 rota protegida de teste
+  fastify.get('/me', { preHandler: authMiddleware }, async (request, reply) => {
+    return request.user;
+  });
 }
